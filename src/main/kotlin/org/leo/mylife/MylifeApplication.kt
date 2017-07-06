ed77832976bf23b4450a387961b298191219871c
Lin (Leo) Lin <leo0102@hotmail.com>
2017-07-06 09:19:18 +0800

    Initial commit

---
+++ src/main/kotlin/org/leo/mylife/MylifeApplication.kt
@@ -0,0 +1,11 @@
+package org.leo.mylife
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MylifeApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(MylifeApplication::class.java, *args)
+}
